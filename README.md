60e45193c1e42fda487b6e66c26d38a527709201
timeeeee <timeeeee@gmail.com>
2017-05-31 22:52:26 -0500

    barebones everything

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # centipede-game
 evolve mixed strategies for the centipede game
+
+After cloning:
+
+# Run `npm install` to install dependencies
+# Run `browserify main.js -o bundle.js` or `npm run-script build` to bundle the javascript
+# Open index.html in a browser.
---
+++ main.js
@@ -0,0 +1,16 @@
+const d3 = require('d3');
+const $ = require('jquery');
+
+const centipede = require('./centipede.js');
+
+
+
+$(function load() {
+    for (let i = 0; i < 10; i++) {
+	$("body").append("<p class='container'></p>");
+    }
+
+    d3.selectAll("p.container").
+	data([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).
+	text(function(d) {return "number" + d; });
+});
